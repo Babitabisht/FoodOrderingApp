554f3ef8ba6b2af466d90a7c0291625b5d0e9c49
babitabisht <babitabisht2018@gmail.com>
2021-06-06 19:30:33 +0530

    designing done

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,24 @@
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
+
+const MealItemForm=(props)=>{
+
+    return (
+        <form  className={classes.form}> 
+            <Input  input={{
+                type : 'number',
+                id : 'amount',
+                min: '1',
+                max : '5',
+                step : '1',
+                defaultValue : '1'
+
+            }} label="Amount" />
+            <button> +Add </button>
+        </form>
+    );
+
+}
+
+
+export default MealItemForm;
